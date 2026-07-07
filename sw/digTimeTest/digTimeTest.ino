cbae2c6e65626455a0a90f6527e2163e25034380
Erik <Ted.ErikssonZ@live.se>
2026-07-07 13:48:54 +0200

    valbar riktning på ljusdep switchState

--- sw/digTimeTest/digTimeTest.ino
+++ sw/digTimeTest/digTimeTest.ino
@@ -1,9 +1,12 @@
+
+// not using function to follow structure, men borde ha det
+
 // globals
 const int SwitchPin = 8;
 unsigned long previousTime = 0;
 int SwitchState = 0;
 int prevSwitchState = 0;
-int led = 2;
+int led = 0;
 
 long interval = 3000; // 6 sec
 
@@ -12,6 +15,14 @@ void setup() {
     pinMode(x,OUTPUT); // declare as outputs
   }
   pinMode(SwitchPin, INPUT);
+  Serial.begin(9600);    
+  SwitchState = digitalRead(SwitchPin);
+  if (SwitchState==0){
+    led = 7;
+    }
+  else{
+    led = 2;
+    }
 }
 
 void loop() {
@@ -19,8 +30,8 @@ void loop() {
   if(currentTime - previousTime > interval){
      previousTime = currentTime;
      digitalWrite(led,HIGH);
-     led++;
-     if(led==7){
+  
+     if( (led==2 && SwitchState==0) || (led==7 && SwitchState==1) ){
         digitalWrite(led,HIGH);
         while(SwitchState == prevSwitchState){
            delay(1000);
@@ -34,6 +45,13 @@ void loop() {
           SwitchState = digitalRead(SwitchPin);// function blink
        }
       }
+
+     if (SwitchState==0){
+        led--;
+     }
+     else {
+       led++;
+     }
   }
   
 SwitchState = digitalRead(SwitchPin);
@@ -41,9 +59,16 @@ SwitchState = digitalRead(SwitchPin);
     for(int x=2;x<8;x++){
         digitalWrite(x, LOW);
         }
-      led =2;
+    if (SwitchState==0){
+      led = 7;
+      }
+     else{
+      led = 2;
+      }
       previousTime = currentTime;
       }
 
   prevSwitchState = SwitchState; // in the end of the loop
+  Serial.println("SwitchState: "); // 0 down, 1 up
+  Serial.println(SwitchState);
 }
